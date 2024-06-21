f671c929b44b414622f2edc1720c8d5f739858af
Jake Shilling <shilling.jake@gmail.com>
2024-06-20 22:30:21 -0400

    Add wrapper to make Effects from commands

--- packages/appsync/src/index.ts
+++ packages/appsync/src/index.ts
@@ -1,1 +1,172 @@
-console.log('Hello World!');
+import * as SDK from "@aws-sdk/client-appsync";
+import * as SmithyClient from "@functorfactory/smithy-client";
+
+export namespace AppSync {
+    export class AppSyncClient extends SmithyClient.Tag(
+        "AppSync",
+        SDK.AppSyncClient
+    )<AppSyncClient>() {}
+
+    export const associateApi = AppSyncClient.command(SDK.AssociateApiCommand);
+    export const associateMergedGraphqlApi = AppSyncClient.command(
+        SDK.AssociateMergedGraphqlApiCommand
+    );
+    export const associateSourceGraphqlApi = AppSyncClient.command(
+        SDK.AssociateSourceGraphqlApiCommand
+    );
+    export const createApiCache = AppSyncClient.command(
+        SDK.CreateApiCacheCommand
+    );
+    export const createApiKey = AppSyncClient.command(SDK.CreateApiKeyCommand);
+    export const createDataSource = AppSyncClient.command(
+        SDK.CreateDataSourceCommand
+    );
+    export const createDomainName = AppSyncClient.command(
+        SDK.CreateDomainNameCommand
+    );
+    export const createFunction = AppSyncClient.command(
+        SDK.CreateFunctionCommand
+    );
+    export const createGraphqlApi = AppSyncClient.command(
+        SDK.CreateGraphqlApiCommand
+    );
+    export const createResolver = AppSyncClient.command(
+        SDK.CreateResolverCommand
+    );
+    export const createType = AppSyncClient.command(SDK.CreateTypeCommand);
+    export const deleteApiCache = AppSyncClient.command(
+        SDK.DeleteApiCacheCommand
+    );
+    export const deleteApiKey = AppSyncClient.command(SDK.DeleteApiKeyCommand);
+    export const deleteDataSource = AppSyncClient.command(
+        SDK.DeleteDataSourceCommand
+    );
+    export const deleteDomainName = AppSyncClient.command(
+        SDK.DeleteDomainNameCommand
+    );
+    export const deleteFunction = AppSyncClient.command(
+        SDK.DeleteFunctionCommand
+    );
+    export const deleteGraphqlApi = AppSyncClient.command(
+        SDK.DeleteGraphqlApiCommand
+    );
+    export const deleteResolver = AppSyncClient.command(
+        SDK.DeleteResolverCommand
+    );
+    export const deleteType = AppSyncClient.command(SDK.DeleteTypeCommand);
+    export const disassociateApi = AppSyncClient.command(
+        SDK.DisassociateApiCommand
+    );
+    export const disassociateMergedGraphqlApi = AppSyncClient.command(
+        SDK.DisassociateMergedGraphqlApiCommand
+    );
+    export const disassociateSourceGraphqlApi = AppSyncClient.command(
+        SDK.DisassociateSourceGraphqlApiCommand
+    );
+    export const evaluateCode = AppSyncClient.command(SDK.EvaluateCodeCommand);
+    export const evaluateMappingTemplate = AppSyncClient.command(
+        SDK.EvaluateMappingTemplateCommand
+    );
+    export const flushApiCache = AppSyncClient.command(
+        SDK.FlushApiCacheCommand
+    );
+    export const getApiAssociation = AppSyncClient.command(
+        SDK.GetApiAssociationCommand
+    );
+    export const getApiCache = AppSyncClient.command(SDK.GetApiCacheCommand);
+    export const getDataSource = AppSyncClient.command(
+        SDK.GetDataSourceCommand
+    );
+    export const getDataSourceIntrospection = AppSyncClient.command(
+        SDK.GetDataSourceIntrospectionCommand
+    );
+    export const getDomainName = AppSyncClient.command(
+        SDK.GetDomainNameCommand
+    );
+    export const getFunction = AppSyncClient.command(SDK.GetFunctionCommand);
+    export const getGraphqlApi = AppSyncClient.command(
+        SDK.GetGraphqlApiCommand
+    );
+    export const getGraphqlApiEnvironmentVariables = AppSyncClient.command(
+        SDK.GetGraphqlApiEnvironmentVariablesCommand
+    );
+    export const getIntrospectionSchema = AppSyncClient.command(
+        SDK.GetIntrospectionSchemaCommand
+    );
+    export const getResolver = AppSyncClient.command(SDK.GetResolverCommand);
+    export const getSchemaCreationStatus = AppSyncClient.command(
+        SDK.GetSchemaCreationStatusCommand
+    );
+    export const getSourceApiAssociation = AppSyncClient.command(
+        SDK.GetSourceApiAssociationCommand
+    );
+    export const getType = AppSyncClient.command(SDK.GetTypeCommand);
+    export const listApiKeys = AppSyncClient.command(SDK.ListApiKeysCommand);
+    export const listDataSources = AppSyncClient.command(
+        SDK.ListDataSourcesCommand
+    );
+    export const listDomainNames = AppSyncClient.command(
+        SDK.ListDomainNamesCommand
+    );
+    export const listFunctions = AppSyncClient.command(
+        SDK.ListFunctionsCommand
+    );
+    export const listGraphqlApis = AppSyncClient.command(
+        SDK.ListGraphqlApisCommand
+    );
+    export const listResolversByFunction = AppSyncClient.command(
+        SDK.ListResolversByFunctionCommand
+    );
+    export const listResolvers = AppSyncClient.command(
+        SDK.ListResolversCommand
+    );
+    export const listSourceApiAssociations = AppSyncClient.command(
+        SDK.ListSourceApiAssociationsCommand
+    );
+    export const listTagsForResource = AppSyncClient.command(
+        SDK.ListTagsForResourceCommand
+    );
+    export const listTypesByAssociation = AppSyncClient.command(
+        SDK.ListTypesByAssociationCommand
+    );
+    export const listTypes = AppSyncClient.command(SDK.ListTypesCommand);
+    export const putGraphqlApiEnvironmentVariables = AppSyncClient.command(
+        SDK.PutGraphqlApiEnvironmentVariablesCommand
+    );
+    export const startDataSourceIntrospection = AppSyncClient.command(
+        SDK.StartDataSourceIntrospectionCommand
+    );
+    export const startSchemaCreation = AppSyncClient.command(
+        SDK.StartSchemaCreationCommand
+    );
+    export const startSchemaMerge = AppSyncClient.command(
+        SDK.StartSchemaMergeCommand
+    );
+    export const tagResource = AppSyncClient.command(SDK.TagResourceCommand);
+    export const untagResource = AppSyncClient.command(
+        SDK.UntagResourceCommand
+    );
+    export const updateApiCache = AppSyncClient.command(
+        SDK.UpdateApiCacheCommand
+    );
+    export const updateApiKey = AppSyncClient.command(SDK.UpdateApiKeyCommand);
+    export const updateDataSource = AppSyncClient.command(
+        SDK.UpdateDataSourceCommand
+    );
+    export const updateDomainName = AppSyncClient.command(
+        SDK.UpdateDomainNameCommand
+    );
+    export const updateFunction = AppSyncClient.command(
+        SDK.UpdateFunctionCommand
+    );
+    export const updateGraphqlApi = AppSyncClient.command(
+        SDK.UpdateGraphqlApiCommand
+    );
+    export const updateResolver = AppSyncClient.command(
+        SDK.UpdateResolverCommand
+    );
+    export const updateSourceApiAssociation = AppSyncClient.command(
+        SDK.UpdateSourceApiAssociationCommand
+    );
+    export const updateType = AppSyncClient.command(SDK.UpdateTypeCommand);
+}
--- packages/smithy/src/index.ts
+++ packages/smithy/src/index.ts
@@ -1,96 +1,161 @@
-import { Client } from "@smithy/smithy-client";
+import {
+    Client,
+    ServiceException,
+    SmithyResolvedConfiguration,
+} from "@smithy/smithy-client";
+import { Command, MetadataBearer } from "@smithy/types";
 import { Context, Effect, Layer, Scope } from "effect";
-
-export interface SmithyShape<
-    Ctor extends new (...args: any) => Client<any, any, any, any>
-> {
-    acquireClient: Effect.Effect<InstanceType<Ctor>, never, Scope.Scope>;
-    useClient: <A, E, R>(
-        use: (client: InstanceType<Ctor>) => Effect.Effect<A, E, R>
-    ) => Effect.Effect<A, E, R>;
-}
-
-export const make = <
-    Ctor extends new (...args: any) => Client<any, any, any, any>
->(
-    ctor: Ctor,
-    ...args: ConstructorParameters<Ctor>
-): SmithyShape<Ctor> => {
-    const acquire = Effect.sync(() => new ctor(...args) as InstanceType<Ctor>);
-    const release = (client: InstanceType<Ctor>) =>
-        Effect.sync(() => client.destroy());
-
-    return {
-        acquireClient: Effect.acquireRelease(acquire, release),
-        useClient: (use) => Effect.acquireUseRelease(acquire, use, release),
-    };
-};
+import { UnknownException } from "effect/Cause";
 
 export interface Tag<
     in out Id,
     in out Ctor extends new (...args: any) => Client<any, any, any, any>
-> extends Context.Tag<Id, SmithyShape<Ctor>> {}
+> extends Context.Tag<Id, InstanceType<Ctor>> {}
 
 export interface TagClass<
     Self,
     Id,
-    Ctor extends new (...args: any) => Client<any, any, any, any>
-> extends Context.TagClass<Self, Id, SmithyShape<Ctor>> {
-    new: (...args: ConstructorParameters<Ctor>) => Context.TagClassShape<Id, SmithyShape<Ctor>>
-    provideService: {
-        <A, E, R>(
-            self: Effect.Effect<A, E, R>,
-            ...args: ConstructorParameters<Ctor>
-        ): Effect.Effect<A, E, Exclude<R, Self>>;
-        (...args: ConstructorParameters<Ctor>): <A, E, R>(
-            self: Effect.Effect<A, E, R>
-        ) => Effect.Effect<A, E, Exclude<R, Self>>;
-    };
-    layer: {
-        (...args: ConstructorParameters<Ctor>): Layer.Layer<Self>;
-    };
+    Ctor extends new (...args: any) => Client<any, any, any, any>,
+    HandlerOptions = InstanceType<Ctor> extends Client<
+        infer H,
+        any,
+        any,
+        any
+    >
+        ? H
+        : never,
+    ClientInput extends object = InstanceType<Ctor> extends Client<
+        any,
+        infer I,
+        any,
+        any
+    >
+        ? I
+        : never,
+    ClientOutput extends MetadataBearer = InstanceType<Ctor> extends Client<
+        any,
+        any,
+        infer O,
+        any
+    >
+        ? O
+        : never,
+    ResolvedClientConfiguration extends SmithyResolvedConfiguration<HandlerOptions> = InstanceType<Ctor> extends Client<
+        any,
+        any,
+        any,
+        infer R
+    >
+        ? R
+        : never
+> extends Context.TagClass<Self, Id, InstanceType<Ctor>> {
+    readonly make: (
+        ...args: ConstructorParameters<Ctor>
+    ) => Effect.Effect<Self, never, Scope.Scope>;
+
+    readonly layer: (
+        ...args: ConstructorParameters<Ctor>
+    ) => Layer.Layer<Self, never, Scope.Scope>;
+
+    readonly command: <
+        CmdCtor extends new (...args: any) => Command<ClientInput, any, ClientOutput, any, ResolvedClientConfiguration>,
+        InputType extends ClientInput = InstanceType<CmdCtor> extends Command<any, infer I, any, any, any> ? I : never,
+        OutputType extends ClientOutput = InstanceType<CmdCtor> extends Command<any, any, any, infer O, any> ? O : never
+    >(
+        ctor: CmdCtor,
+    ) => (
+        input: InputType
+    ) => Effect.Effect<OutputType, ServiceException | UnknownException, Self>;
 }
 
-export const provideService = <
-    Self,
-    Id,
-    Ctor extends new (...args: any) => Client<any, any, any, any>
->(
-    tag: Tag<Self, Ctor>,
-    ctor: Ctor
-): TagClass<Self, Id, Ctor>["provideService"] => {
-    const f = <A, E, R>(
-        self: Effect.Effect<A, E, R>,
-        ...args: ConstructorParameters<Ctor>
-    ): Effect.Effect<A, E, Exclude<R, Self>> => {
-        return Effect.provideService(self, tag, make(ctor, ...args));
+export const make =
+    <Ctor extends new (...args: any) => Client<any, any, any, any>>(
+        ctor: Ctor
+    ) =>
+    (...args: ConstructorParameters<Ctor>) => {
+        const getInstance = () => new ctor(...args) as InstanceType<Ctor>;
+
+        const acquire = Effect.sync(getInstance);
+
+        const release = (instance: InstanceType<Ctor>) =>
+            Effect.sync(() => instance.destroy());
+
+        return Effect.acquireRelease(acquire, release);
     };
 
-    return ((...args: any[]) => {
-        if (Effect.isEffect(args[0])) {
-            const self = args[0] as Effect.Effect<any, any, any>;
-            const rest = args.slice(1) as ConstructorParameters<Ctor>;
-            return f(self, ...rest);
-        } else {
-            const rest = args as ConstructorParameters<Ctor>;
-            return (self: Effect.Effect<any>) => f(self, ...rest);
-        }
-    }) as any;
-};
+export const layer =
+    <
+        Self,
+        Id,
+        Ctor extends new (...args: any) => Client<any, any, any, any>
+    >(
+        tag: Context.TagClass<Self, Id, InstanceType<Ctor>>,
+        ctor: Ctor
+    ) =>
+    (
+        ...args: ConstructorParameters<Ctor>
+    ): Layer.Layer<Self, never, Scope.Scope> =>
+        Layer.effect(tag, make(ctor)(...args));
 
-export const layer = <
-    Self,
-    Id,
-    Ctor extends new (...args: any) => Client<any, any, any, any>
->(
-    tag: Tag<Self, Ctor>,
-    ctor: Ctor
-): TagClass<Self, Id, Ctor>["layer"] => (
-    ...args
-) => Layer.succeed(
-    tag,
-    make(ctor, ...args),
-);
+export const command = <
+        Service extends TagClass<any, any, any>,
+        Ctor extends new (...args: any) => Client<any, any, any, any> = Service extends TagClass<any, any, infer C> ? C : never,
+    HandlerOptions = InstanceType<Ctor> extends Client<
+        infer H,
+        any,
+        any,
+        any
+    >
+        ? H
+        : never,
+    ClientInput extends object = InstanceType<Ctor> extends Client<
+        any,
+        infer I,
+        any,
+        any
+    >
+        ? I
+        : never,
+    ClientOutput extends MetadataBearer = InstanceType<Ctor> extends Client<
+        any,
+        any,
+        infer O,
+        any
+    >
+        ? O
+        : never,
+    ResolvedClientConfiguration extends SmithyResolvedConfiguration<HandlerOptions> = InstanceType<Ctor> extends Client<
+        any,
+        any,
+        any,
+        infer R
+    >
+        ? R
+    : never
+    >(service: Service) => <
+        CmdCtor extends new (...args: any) => Command<ClientInput, any, ClientOutput, any, ResolvedClientConfiguration>,
+        InputType extends ClientInput = InstanceType<CmdCtor> extends Command<any, infer I, any, any, any> ? I : never,
+        OutputType extends ClientOutput = InstanceType<CmdCtor> extends Command<any, any, any, infer O, any> ? O : never
+    >(
+        ctor: CmdCtor
+    ) => (
+        input: InputType
+    ): Effect.Effect<OutputType, ServiceException | UnknownException, Service> => Effect.gen(function* () {
+        const client: InstanceType<Ctor> = yield* service;
+        const cmd = new ctor(input) as InstanceType<CmdCtor>;
+
+        return yield* Effect.tryPromise({
+            try: () => client.send(cmd),
+            catch: (err) => {
+                if (err instanceof ServiceException) {
+                    return err;
+                } else {
+                    return new UnknownException(err);
+                }
+            },
+        });
+    })
+
 
 export const Tag =
     <
@@ -101,14 +166,18 @@ export const Tag =
         ctor: Ctor
     ) =>
     <Self>(): TagClass<Self, Id, Ctor> => {
-        const TagClass = Context.Tag(id)<Self, SmithyShape<Ctor>>();
+        const TagClass = Context.Tag(id)<Self, InstanceType<Ctor>>();
         const parent = Object.getPrototypeOf(TagClass);
         const proto: TagClass<Self, Id, Ctor> = {
             ...parent,
-            provideService: provideService(TagClass, ctor),
+            make: make(ctor),
             layer: layer(TagClass, ctor),
-        }
+            command: command(TagClass as any),
+        };
 
         Object.setPrototypeOf(TagClass, proto);
         return TagClass as any;
     };
+
+export type * as SmithyClient from '@smithy/smithy-client';
+export type * as SmithyTypes from '@smithy/types';
